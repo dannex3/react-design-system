af8f35dee858b406f5f48e95c04d4ed8f35d2f69
dannex3 <danielle.souza@calindra.com.br>
2022-10-14 02:23:22 -0300

    test

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/react-design-system.git'
+      config.base = '/react-design-system.git/'
     }
 
     return config
