04ffcf8c10f35180527b601a7d70e4b3e92286ab
dannex3 <danielle.souza@calindra.com.br>
2022-10-14 00:36:17 -0300

    Create react app, config tailwindcss, config storybook, create components

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css'
+
+export function App() {
+  return (
+    <>
+      <span
+        className='font-bold text-5xl text-violet-500'
+      >
+        Hello World
+      </span>
+    </>
+  )
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx'
+  ],
+  theme: {
+    colors: {
+      gray: {
+        900: "#121214",
+        800: "#202024",
+        400: "#7C7C8A",
+        200: "#C4C4CC",
+        100: "#E1E1E6",
+      },
+
+      cyan: {
+        500: "#81D8F7",
+        300: "#B1EBFF",
+      },
+
+      black: "#000",
+      white: "#FFF",
+      transparent: "transparent",
+    },
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      "2xl": 32,
+    },
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif'
+      },
+    },
+  },
+  plugins: [],
+}
